f8eb142d9897e4e9503f1dee44451dc8d1beb642
ugo <s_per00@outlook.com>
2020-05-15 10:20:25 +0300

    impoved looks

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const HEIGHT = 600;
 root.setAttribute('height', HEIGHT)
 root.setAttribute('width', WIDTH)
 
-function renderArr(arr, multiplyer = 1, color='orchid') {
+function renderArr(arr, multiplyer = 1, color='	#ffdfba') {
     root.innerHTML = '';
     const width =  WIDTH/arr.length;
     arr.forEach((size, index) => {
@@ -36,8 +36,8 @@ function* bubbleSort(arr) {
         counter++;
         while(index < arr.length-1*counter) {
             if(dataSizeSort(arr, index)) {
-                arr[index+1].lastChild.setAttribute('style', 'fill: red');
-                arr[index].lastChild.setAttribute('style', 'fill: red');
+                arr[index+1].lastChild.setAttribute('style', 'fill: 	#ffb3ba');//red
+                arr[index].lastChild.setAttribute('style', 'fill: 	#ffb3ba');//red
                 swap(arr, index);
                 isSwapped = true;
             };
@@ -51,7 +51,7 @@ function* bubbleSort(arr) {
         
         if (!isSwapped) {
             for(let i = 0; i < counter; i++) {
-                arr[i].lastChild.setAttribute('style', 'fill: blue');
+                arr[i].lastChild.setAttribute('style', 'fill: #bae1ff');//blue
             }
             return arr
         };
@@ -83,47 +83,78 @@ function renderSortStep(id, isSwapped) {
     
 
     if(prev) {
-        prev.lastChild.setAttribute('style', 'fill: orchid');
+        prev.lastChild.setAttribute('style', 'fill: 	#ffdfba');
     };
-    if(arrElements[id+1] && arrElements[id+1].getAttribute('style')!=='fill: blue') {
-        arrElements[id+1].lastChild.setAttribute('style', 'fill: green');
+    if(arrElements[id+1] && arrElements[id+1].getAttribute('style')!=='fill: 	#bae1ff') {//blue
+        arrElements[id+1].lastChild.setAttribute('style', 'fill: 	#baffc9');//green
     }
     if(arrElements[id].getAttribute('style')!=='fill: blue') {
-        arrElements[id].lastChild.setAttribute('style', 'fill: green');
+        arrElements[id].lastChild.setAttribute('style', 'fill: 	#baffc9');//green
     }
     
     if(lastElementIndex) {
-        arrElements[lastElementIndex].lastChild.setAttribute('style', 'fill: blue');
+        arrElements[lastElementIndex].lastChild.setAttribute('style', 'fill: 	#bae1ff');//blue
     }
 }
 
+function generateArr(size) {
+    newArr = [];
+    for (let i = 0; i < size; i++) {
+        newArr.push(Math.random()*HEIGHT);
+    }
+    return newArr;
+}
+
 const arr1 = [2, 10, 7, 3, 1, 6, 20, 12, 13, 11, 5];
-renderArr(arr1);
+const arr2 = generateArr(30);
+renderArr(arr2);
 
 let arrElements = [...document.querySelectorAll('g')];
 
-const delay = 100;
+const sortBtn = document.querySelector('button');
+const arraySizeRange = document.getElementById('array-size-range');
 
-bubble = bubbleSort(arrElements);
-const interval = setInterval( ()=>{
-    new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve(bubble.next())
-        }, delay*0.7)
-    })
-    .then((data)=>{
-        const {value, done} = data;
-        console.log(done)
-        if(done) {
-            clearInterval(interval);
-            return;
-        };
-        new Promise((resolve, reject) => {
+sortBtn.addEventListener('click', sortHandler);
+arraySizeRange.addEventListener('input', arrSizeHandler);
+let delay = 600;
+
+function arrSizeHandler(event) {
+    const volume = parseInt(event.target.value);
+    const arr = generateArr(volume);
+    renderArr(arr);
+    arrElements = [...document.querySelectorAll('g')];
+}
+
+function sortHandler() {
+    sortBtn.disabled = true;
+    arraySizeRange.disabled = true;
+    sortBtn.innerText = 'sorting...'
+    bubble = bubbleSort(arrElements);
+    const interval = setInterval( ()=>{
+        new Promise((resolve, reject)=>{
             setTimeout(()=>{
-                resolve(renderSortStep(...value));
+                resolve(bubble.next())
             }, delay*0.7)
-            
-        }).then(()=> {return});
-    })
-}, delay);
+        })
+        .then((data)=>{
+            const {value, done} = data;
+            console.log(done)
+            if(done) {
+                clearInterval(interval);
+                sortBtn.disabled = false;
+                arraySizeRange.disabled = false;
+                return;
+            };
+            new Promise((resolve, reject) => {
+                setTimeout(()=>{
+                    resolve(renderSortStep(...value));
+                }, delay*0.7)
+                
+            }).then(()=> {return});
+        })
+    }, delay);
+}
+
+
+
 
