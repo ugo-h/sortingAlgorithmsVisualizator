f8356a6d4ebb46bcf1e8d782f18f41d2a86f7c21
ugo-h <s_per00@outlook.com>
2020-10-07 19:45:31 +0300

    commit before refactoring

--- src/Algorithms/bubbleSort.js
+++ src/Algorithms/bubbleSort.js
@@ -10,7 +10,10 @@ export function* bubbleSortG(arr) {
     let counter = 0;
     for(let j = 0; j < arr.length-1; j++)  {
         for(let i = 0; i < arr.length-j-1; i++) {
-            if(arr[arr.length-j]) arr[arr.length-j].color = 'green';
+            if(arr[arr.length-j]){
+                 arr[arr.length-j].color = '#99b6d8';
+                 arr[arr.length-j-1].color = '#ffdfba';
+            }
             let swaped = false;
             counter++;
             if(counter> 1000) throw new Error('Overflow')
